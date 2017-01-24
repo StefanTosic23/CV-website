e3e25db8cba0b1372d9f07aa1009a693795a89df
Stefan Tosic <stefantosic32@gmail.com>
2017-01-24 22:08:20 +0100

    changing the arrows so they respond properly

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,24 @@
+$(document).ready(function(){
 
 var accordion = $('.accordion');
 
 accordion.find('dd').hide();
+    
 
 accordion.find('dt').on('click', function(){
     
-    $(this).toggleClass('open').next('dd').slideToggle().siblings('dd:visible').slideUp().prev('dt').removeClass('open');
-   // $(this).find('.skill-container').toggleClass('no-margin');
+    $(this).toggleClass('open').next('dd').slideToggle().siblings('dd:visible').slideUp().prev('dt').removeClass('open');   
     
     $('i', this).toggleClass('ion-chevron-down ion-chevron-up');
     
+    var otherArrows = $(this).siblings('dt').find('i');
+    
+    if(otherArrows.hasClass('ion-chevron-up')){
+        otherArrows.removeClass('ion-chevron-up');
+        otherArrows.addClass('ion-chevron-down');
+    }
     
     
+});
+
 });
